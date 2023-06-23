1a3e26a801027b6d6235128b045f8a322077e545
FonzyLima <Alfonso_lima@dlsu.edu.ph>
2023-06-23 19:47:45 +0800

    nav update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import {AiOutlineClose} from 'react-icons/ai'
 const Navbar = () => {
     return(
         <>
-        <nav className="w-full flex md:justify-center">
-
+        <nav className="w-full flex md:justify-center justify-between">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                
+            </div>
         </nav>
         </>
     )
